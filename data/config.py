3cebe0c7bcb672fdb3a774938f1bb18f822559ab
Alisher959 <85029201+Alisher959@users.noreply.github.com>
2022-03-29 23:11:44 +0500

    'first_commit'

---
+++ data/config.py
@@ -0,0 +1,12 @@
+from environs import Env
+
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = '5047672576:AAG41CD2eMORf5vTY8ZuJOp5jG5sUW9iuZk'  # Bot toekn
+ADMINS = ['5069753238']  # adminlar ro'yxati
+IP = 'ip'  # Xosting ip manzili
+
+
